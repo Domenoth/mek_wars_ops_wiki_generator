67f37782128171f53dab7e1ce9547c3fa6e360f2
Kent K <kent.krenrich@qualitysmith.com>
2013-02-16 20:10:23 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MekWarsOpsWikiGenerator::Application.config.secret_token = '59c6ca8cb0e4cbb4ae30a1625a9c3e85617d3f466a6b83be4bd2225fce99fc2af5490104f685701aadc7a9c28197f8e5f176117f4d83e4874d89531fa500ae21'
