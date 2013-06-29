983db7a88b454b823c6de3626995e0053186b32c
Kent K <bardbowman@aol.com>
2013-06-29 12:40:59 +0400

    Moved to accepting up to 15 op-files at once outputting a zip for download.

--- app/controllers/generate_wiki_controller.rb
+++ app/controllers/generate_wiki_controller.rb
@@ -3,10 +3,15 @@ class GenerateWikiController < ApplicationController
   def form
   end
 
+  def another_file
+  end
+
   def upload
-    @file = params[:op_file]
-    if @file
-      parse_file
+    @files = params[:op_file]
+    if @files.present?
+      io_string = parse_files
+      io_string.rewind
+      send_data io_string.read, filename: "wiki_ops.zip", type: "text/zip"
     else
       redirect_to root_path, notice: "Please Upload a File"
     end
@@ -14,13 +19,30 @@ class GenerateWikiController < ApplicationController
 
   private
 
-  def parse_file
+  def parse_files
+    Zip::ZipOutputStream::write_buffer do |zipfile|
+      @files.each do |op_file|
+        if op_name( op_file )
+          zipfile.put_next_entry( op_name( op_file ) )
+          zipfile.write( parse_file( op_file ) )
+        end
+      end
+    end
+  end
+
+  def parse_file( op_file )
     @passed_in_settings = {}
-    @file.read.each_line do |line|
+    op_file.read.each_line do |line|
       key, value = line.split( "=" )
       if key.present? and value.present?
         @passed_in_settings[key.strip] = value.strip
       end
     end
+    render_to_string :upload
+  end
+
+  def op_name( op_file )
+    match = op_file.original_filename.match /\A(.*)\.txt\Z/
+    match.try( :[], 1 )
   end
 end
